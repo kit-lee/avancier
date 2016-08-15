df94961023bf02c154f9d460da7074136b8702a5
Kit Lee <kit_21cn@21cn.com>
2016-08-15 11:45:35 +0800

    初始化

---
+++ src/main/java/com/muses/AvancierApplication.java
@@ -0,0 +1,12 @@
+package com.muses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvancierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvancierApplication.class, args);
+	}
+}
